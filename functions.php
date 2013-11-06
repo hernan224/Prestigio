348c80bb737fedd84d81f56b1617a51a27bb40ab
agustinlopini <juanagustin89@gmail.com>
2013-11-06 01:52:05 -0300

    Inicio de estilo visual de la página.
    Agregado Google Fonts, removido líneas y funciones innecesarias, y
    comienzo de personalización de la hoja de estilos.

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function prestigio_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	/**
 	 * This theme uses wp_nav_menu() in one location.
@@ -52,14 +52,6 @@ function prestigio_setup() {
 	 * Enable support for Post Formats
 	 */
 	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
-
-	/**
-	 * Setup the WordPress core custom background feature.
-	 */
-	add_theme_support( 'custom-background', apply_filters( 'prestigio_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
 }
 endif; // prestigio_setup
 add_action( 'after_setup_theme', 'prestigio_setup' );
@@ -85,24 +77,11 @@ add_action( 'widgets_init', 'prestigio_widgets_init' );
 function prestigio_scripts() {
 	wp_enqueue_style( 'prestigio-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'prestigio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_register_style('prestigio-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400,700');
+	wp_enqueue_style( 'prestigio-fonts');
 
-	wp_enqueue_script( 'prestigio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-
-	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'prestigio-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'prestigio_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
