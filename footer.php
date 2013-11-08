2469d42cae3740984d1bbfdcd3ba993244cf7722
agustinlopini <juanagustin89@gmail.com>
2013-11-07 23:24:59 -0300

    Inicio del footer
    Comienzo del desarrollo del footer.
    Simplificación visual.

--- footer.php
+++ footer.php
@@ -12,10 +12,30 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
+			
 			<?php do_action( 'prestigio_credits' ); ?>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'prestigio' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'prestigio' ), 'Prestigio', '<a href="http://imotionconsulting.com.ar" rel="designer">IMotion Consulting</a>' ); ?>
+			
+			<div class="site-info-logo">
+			    <a href="<?php bloginfo( 'home' ); ?>">Spinelli Prestigio</a>
+			</div>
+			
+			<div class="site-info-contact">
+			    <ul>
+			        <li>Rodriguez 315 - Bahía Blanca</li>
+			        <li>(0291) 454-8845 / 452-5289</li>
+			        <li><a href="<?php bloginfo( 'url' ); ?>/contacto">Contacto</a></li>
+			        <li><a href="#">Data Fiscal</a></li>
+			    </ul>
+			</div>
+			
+			<nav class="footer-navigation">
+			    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav>
+
+			<div class="site-info-copyright">
+				<p>Todos los derechos reservados <?php echo date( 'Y' ); ?>. Desarrollo por <a href="http://imotionconsulting.com.ar/" target="_blank">IMotion Consulting</a></p>
+			</div>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
