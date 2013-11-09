4d7c5558fae9033a9d8a62d630f9f961f4aa4b2b
hernan224 <hernan224@gmail.com>
2013-11-08 22:30:17 -0300

    Terminada la maqueta de la Propiedad individual

--- __maquetas__/js/script.js
+++ __maquetas__/js/script.js
@@ -1,7 +1,50 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: HERNAN
- * Date: 06/11/13
- * Time: 16:18
- * To change this template use File | Settings | File Templates.
- */
+jQuery(document).ready(function() {
+
+    /** Silder galería propiedad individual **/
+    jQuery("#galeria-propiedad").owlCarousel({
+        singleItem : true,
+
+        //Autoplay
+        autoPlay : true,
+        stopOnHover : true,
+
+        // Navigation
+        navigation : true,
+        navigationText : ["<",">"],
+        //rewindNav : false,
+
+        //Auto height
+        autoHeight : true,
+
+        //Pagination
+        pagination : true,
+        paginationNumbers: true
+    });
+
+    /** Fancybox fotos galería propiedad individual **/
+    jQuery(".fancy-gal").fancybox({
+        openEffect	: 'none',
+        closeEffect	: 'none'
+    });
+
+    /** Fancybox para mostrar google maps
+     * (el boton debe llevar la clase fancybox.iframe para que funcione) **/
+    jQuery('.fancy-map').fancybox({
+        maxWidth	: 960,
+        maxHeight	: 600,
+        fitToView	: false,
+        width		: '70%',
+        height		: '70%',
+        autoSize	: false,
+        closeClick	: false,
+        openEffect	: 'none',
+        closeEffect	: 'none'
+    });
+
+    /** Función que lanza la galería de fotos **/
+    jQuery('.fancy-gal_btn').click(function(event) {
+        event.preventDefault();
+        jQuery('#galeria-propiedad .fancy-gal').first().trigger("click");
+    });
+
+});
