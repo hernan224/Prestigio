c56a6c86d8bb46b8579173b2364febf039e75e57
agustinlopini <juanagustin89@gmail.com>
2013-11-20 22:33:25 -0300

    Más styling
    Eliminado plugin de Gallery Slideshow (obsoleto)
    Agrandado ancho de página a 1040px
    Reacomodo de todas las secciones de la web para que se adapte
    Nuevo fondo del sitio
    Más styling del sitio en general
    Revisión de algunos códigos y limpieza.

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 620; /* pixels */
+	$content_width = 665; /* pixels */
 
 if ( ! function_exists( 'prestigio_setup' ) ) :
 /**
